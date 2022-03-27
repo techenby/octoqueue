16a022b56859053ec64b7bf8e3a8cf241fb68838
Andy Newhouse <hi@andymnewhouse.me>
2022-03-26 21:09:02 -0500

    Add placeholders

--- resources/views/livewire/printers/form.blade.php
+++ resources/views/livewire/printers/form.blade.php
@@ -5,25 +5,31 @@
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="name" value="{{ __('Printer Name') }}" />
-            <x-jet-input id="name" type="text" class="block w-full mt-1" wire:model="printer.name" autofocus />
+            <x-jet-input id="name" type="text" class="block w-full mt-1" wire:model="printer.name" placeholder="Friendly name for printer" autofocus />
             <x-jet-input-error for="printer.name" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="model" value="{{ __('Model') }}" />
-            <x-jet-input id="model" type="text" class="block w-full mt-1" wire:model="printer.model" />
+            <x-jet-input id="model" type="text" class="block w-full mt-1" wire:model="printer.model" placeholder="Ender 3 Pro" />
             <x-jet-input-error for="printer.model" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="url" value="{{ __('Url') }}" />
-            <x-jet-input id="url" type="url" class="block w-full mt-1" wire:model="printer.url" />
+            <div class="relative flex items-stretch flex-grow mt-1 focus-within:z-10">
+                <x-jet-input id="url" type="url" class="block w-full rounded-r-none" wire:model="printer.url" placeholder="https://octoeverywhere.com/shared-url" />
+                <a href="/docs/where-to-get-printer-url" class="relative inline-flex items-center px-4 py-2 -ml-px space-x-2 text-sm font-medium text-gray-700 border border-gray-300 dark:text-gray-400 dark:border-gray-700 rounded-r-md bg-gray-50 dark:bg-gray-850 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
+                    <x-heroicon-s-question-mark-circle class="w-5 h-5 text-gray-400" />
+                    <span class="sr-only">Where to get printer url?</span>
+                </a>
+            </div>
             <x-jet-input-error for="printer.url" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="api_key" value="{{ __('API Key') }}" />
-            <x-jet-input id="api_key" type="password" class="block w-full mt-1" wire:model="printer.api_key" />
+            <x-jet-input id="api_key" type="password" class="block w-full mt-1" wire:model="printer.api_key" placeholder="String from OctoPrint" />
             <x-jet-input-error for="printer.api_key" class="mt-2" />
         </div>
 
