06769babd3950f2958d4546da1c1e532f034f365
Andy Newhouse <hi@andymnewhouse.me>
2022-03-26 21:09:41 -0500

    Move rounded corners

--- resources/views/vendor/jetstream/components/form-section.blade.php
+++ resources/views/vendor/jetstream/components/form-section.blade.php
@@ -9,15 +9,15 @@
     @endif
 
     <div class="mt-5 md:mt-0 md:col-span-2">
-        <form wire:submit.prevent="{{ $submit }}">
-            <div class="px-4 py-5 bg-white dark:bg-gray-800 sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
+        <form wire:submit.prevent="{{ $submit }}" class="overflow-hidden rounded-md dark:border-gray-700 dark:border">
+            <div class="px-4 py-5 bg-white shadow dark:bg-gray-800 sm:p-6">
                 <div class="grid grid-cols-6 gap-6">
                     {{ $form }}
                 </div>
             </div>
 
             @if (isset($actions))
-                <div class="flex items-center justify-end px-4 py-3 text-right shadow bg-gray-50 dark:bg-gray-850 sm:px-6 sm:rounded-bl-md sm:rounded-br-md">
+                <div class="flex items-center justify-end px-4 py-3 text-right shadow bg-gray-50 dark:bg-gray-850 sm:px-6">
                     {{ $actions }}
                 </div>
             @endif
