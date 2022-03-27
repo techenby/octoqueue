60d9c07db4630c48e155666b3c39d29619258280
Andy Newhouse <hi@andymnewhouse.me>
2022-03-26 21:08:10 -0500

    Revert to default Jetstream layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,194 +25,26 @@
 <body class="h-full font-sans antialiased">
     <x-jet-banner />
 
-    <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
-    <div class="fixed inset-0 z-40 flex md:hidden" role="dialog" aria-modal="true">
-        <!--
-        Off-canvas menu overlay, show/hide based on off-canvas menu state.
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        @livewire('navigation-menu')
 
-        Entering: "transition-opacity ease-linear duration-300"
-            From: "opacity-0"
-            To: "opacity-100"
-        Leaving: "transition-opacity ease-linear duration-300"
-            From: "opacity-100"
-            To: "opacity-0"
-        -->
-        <div class="fixed inset-0 bg-gray-600 bg-opacity-75" aria-hidden="true"></div>
+        <!-- Page Heading -->
+        @isset ($header)
+            <header class="bg-white shadow dark:bg-gray-800 dark:border-b dark:border-gray-700">
+                <div class="flex items-center justify-between px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <h1 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">{{ $header }}</h1>
 
-        <!--
-        Off-canvas menu, show/hide based on off-canvas menu state.
-
-        Entering: "transition ease-in-out duration-300 transform"
-            From: "-translate-x-full"
-            To: "translate-x-0"
-        Leaving: "transition ease-in-out duration-300 transform"
-            From: "translate-x-0"
-            To: "-translate-x-full"
-        -->
-        <div class="relative flex flex-col flex-1 w-full max-w-xs bg-white dark:bg-gray-800">
-            <!--
-                Close button, show/hide based on off-canvas menu state.
-
-                Entering: "ease-in-out duration-300"
-                From: "opacity-0"
-                To: "opacity-100"
-                Leaving: "ease-in-out duration-300"
-                From: "opacity-100"
-                To: "opacity-0"
-            -->
-            <div class="absolute top-0 right-0 pt-2 -mr-12">
-                <button type="button" class="flex items-center justify-center w-10 h-10 ml-1 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <span class="sr-only">Close sidebar</span>
-                    <x-heroicon-o-x class="w-6 h-6 text-white" />
-                </button>
-            </div>
-
-            <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-                <div class="flex items-center flex-shrink-0 px-4">
-                    <x-jet-application-logo class="w-auto h-8" />
-                </div>
-                <nav class="px-2 mt-5 space-y-1">
-                    <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
-                    <a href="#" class="flex items-center px-2 py-2 text-base font-medium text-gray-900 bg-gray-100 rounded-md group">
-                        <!--
-                        Heroicon name: outline/home
-
-                        Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
-                        -->
-                        <svg class="flex-shrink-0 w-6 h-6 mr-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                        </svg>
-                        Dashboard
-                    </a>
-
-                    <a href="#" class="flex items-center px-2 py-2 text-base font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 group">
-                        <!-- Heroicon name: outline/users -->
-                        <svg class="flex-shrink-0 w-6 h-6 mr-4 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                        </svg>
-                        Team
-                    </a>
-
-                    <a href="#" class="flex items-center px-2 py-2 text-base font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 group">
-                        <!-- Heroicon name: outline/folder -->
-                        <svg class="flex-shrink-0 w-6 h-6 mr-4 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
-                        </svg>
-                        Projects
-                    </a>
-
-                    <a href="#" class="flex items-center px-2 py-2 text-base font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 group">
-                        <!-- Heroicon name: outline/calendar -->
-                        <svg class="flex-shrink-0 w-6 h-6 mr-4 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                        </svg>
-                        Calendar
-                    </a>
-
-                    <a href="#" class="flex items-center px-2 py-2 text-base font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 group">
-                        <!-- Heroicon name: outline/inbox -->
-                        <svg class="flex-shrink-0 w-6 h-6 mr-4 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-                        </svg>
-                        Documents
-                    </a>
-
-                    <a href="#" class="flex items-center px-2 py-2 text-base font-medium text-gray-600 rounded-md hover:bg-gray-50 hover:text-gray-900 group">
-                        <!-- Heroicon name: outline/chart-bar -->
-                        <svg class="flex-shrink-0 w-6 h-6 mr-4 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                        </svg>
-                        Reports
-                    </a>
-                </nav>
-            </div>
-            <div class="flex flex-shrink-0 p-4 border-t border-gray-200 dark:border-gray-700">
-                <a href="#" class="flex-shrink-0 block group">
-                    <div class="flex items-center">
-                        <div>
-                            <img class="inline-block w-10 h-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                        </div>
-                        <div class="ml-3">
-                            <p class="text-base font-medium text-gray-700 group-hover:text-gray-900">Tom Cook</p>
-                            <p class="text-sm font-medium text-gray-500 group-hover:text-gray-700">View profile</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-
-        <div class="flex-shrink-0 w-14">
-            <!-- Force sidebar to shrink to fit close icon -->
-        </div>
-    </div>
-
-    <!-- Static sidebar for desktop -->
-    <div class="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-        <!-- Sidebar component, swap this element with another sidebar if you like -->
-        <div class="flex flex-col flex-1 min-h-0 bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-800">
-            <div class="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
-                <div class="flex items-center flex-shrink-0 px-4">
-                    <x-jet-application-logo class="w-auto h-8 text-gray-900 dark:text-gray-200" />
-                </div>
-                <nav class="flex-1 px-2 mt-5 space-y-4 bg-white dark:bg-gray-800">
-                    <x-team-dropdown />
-
-                    <div class="space-y-1">
-                        @foreach(config('nav.app') as $link)
-                            @isset($link['route'])
-                            <x-nav-link :href="route($link['route'])" :icon="$link['icon']" :active="request()->routeIs($link['route'])">{{ $link['name'] }}</x-galaxy.nav-link>
-                            @else
-                            <span class="block pt-8 pb-2 pl-2 text-sm tracking-wide text-gray-700 uppercase dark:text-gray-400">{{ $link['name'] }}</span>
-                            @endif
-                        @endforeach
-                    </div>
-                </nav>
-            </div>
-            <div class="flex flex-shrink-0 p-4 border-t border-gray-200 dark:border-gray-700">
-                <a href="#" class="flex-shrink-0 block w-full group">
-                    <div class="flex items-center">
-                        <div>
-                            <img class="inline-block rounded-full h-9 w-9" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                        </div>
-                        <div class="ml-3">
-                            <p class="text-sm font-medium text-gray-700 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-gray-200">Tom Cook</p>
-                            <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700 dark:text-gray-400 dark:group-hover:text-gray-400">View profile</p>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <!-- Main Content -->
-    <div class="flex flex-col flex-1 md:pl-64">
-        <div class="sticky top-0 z-10 pt-1 pl-1 bg-gray-100 md:hidden sm:pl-3 sm:pt-3">
-            <button type="button" class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                <span class="sr-only">Open sidebar</span>
-                <x-heroicon-o-menu class="w-6 h-6" />
-            </button>
-        </div>
-        <main class="flex-1">
-            <div class="space-y-8">
-                <div class="p-4 mx-auto space-y-4 shadow max-w-7xl sm:p-6 md:p-8 bg-gray-50 dark:bg-gray-850">
-                    @isset($breadcrumbs)
-                    <x-breadcrumbs :breadcrumbs="$breadcrumbs" />
+                    @isset($action)
+                    {{ $action }}
                     @endif
-                    <div class="md:flex md:items-center md:justify-between">
-                        <div class="flex-1 min-w-0">
-                            <h1 class="text-2xl font-bold leading-7 text-gray-900 dark:text-gray-200 sm:text-3xl sm:truncate">{{ $header }}</h1>
-                        </div>
-                        @isset($action)
-                        <div class="flex flex-shrink-0 mt-4 md:mt-0 md:ml-4">
-                            {{ $action }}
-                        </div>
-                        @endif
-                    </div>
                 </div>
+            </header>
+        @endif
 
-                <div class="px-4 pb-12 mx-auto max-w-7xl sm:px-6 md:px-8">
-                    {{ $slot }}
-                </div>
+        <!-- Page Content -->
+        <main>
+            <div class="py-10 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                {{ $slot }}
             </div>
         </main>
     </div>
