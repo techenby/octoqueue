e0aef44bdfc8bd278f13075e876a0b7386ef0795
Andy Newhouse <hi@andymnewhouse.me>
2022-03-26 21:24:43 -0500

    Update input to current tailwind ui styles

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -5,7 +5,7 @@
 
 @php
     $label = 'inline-flex items-center px-3 text-gray-500 border border-gray-300 dark:border-gray-700 dark:bg-gray-800 bg-gray-50 dark:text-gray-400';
-    $classes = 'border-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 focus:border-blue-300 dark:focus:border-blue-700 focus:ring focus:ring-blue-200 dark:focus:ring-blue-700 focus:ring-opacity-50'
+    $classes = 'block w-full dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200 shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300'
 @endphp
 
 @if ($leading && $trailing)
@@ -33,5 +33,5 @@
     </span>
 </div>
 @else
-<input {{ $attributes->merge(['type' => 'text', 'class' => $classes . 'shadow-sm']) }} >
+<input {{ $attributes->merge(['type' => 'text', 'class' => $classes . 'shadow-sm rounded-md']) }} >
 @endif
