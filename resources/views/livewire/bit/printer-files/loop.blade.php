cba1b2b433ea4de0aea94a5ffa970cad0b1b6d84
Andy Newhouse <hi@andymnewhouse.me>
2022-03-29 13:21:09 -0500

    Lint

--- resources/views/livewire/bit/printer-files/loop.blade.php
+++ resources/views/livewire/bit/printer-files/loop.blade.php
@@ -1,5 +1,5 @@
 <div class="{{ isset($level) ? 'ml-4' : '' }}">
-    @foreach($files as $file)
+    @foreach ($files as $file)
         @includeWhen($file['type'] === 'folder', 'livewire.bit.printer-files.folder', ['folder' => $file])
         @includeWhen($file['type'] === 'machinecode', 'livewire.bit.printer-files.file', ['file' => $file])
     @endforeach
