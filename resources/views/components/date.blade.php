3802ad4be99dd00ca2c4a8e6541ad4b26be136f1
Andy Newhouse <hi@andymnewhouse.me>
2022-04-01 09:26:17 -0500

    Add more features to date

--- resources/views/components/date.blade.php
+++ resources/views/components/date.blade.php
@@ -1,12 +1,19 @@
 @props([
     'format' => 'Y-m-d',
+    'diff' => false,
     'date' => false,
 ])
 
 @php
     $timezone = session('timezone');
+
+    if ($diff) {
+        $formatted = $date->timezone($timezone)->diffForHumans();
+    } else {
+        $formatted = $date->timezone($timezone)->format($format);
+    }
 @endphp
 
 <span {{ $attributes }} >
-    {{ $date->timezone($timezone)->format($format) }}
+    {{ $formatted }}
 </span>
