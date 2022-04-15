a7bf5bfe48497284c7af6ac5485e1155dc21a344
Andy Newhouse <hi@andymnewhouse.me>
2022-04-15 10:16:42 -0500

    Add collapsing/expanding

--- resources/views/livewire/bit/printer-files/folder.blade.php
+++ resources/views/livewire/bit/printer-files/folder.blade.php
@@ -1,10 +1,12 @@
 @if ($folder['children'])
-<div>
-    <div class="flex items-center px-2 py-1 space-x-2">
+<div x-data="{open: false}">
+    <button type="button" @click="open = !open" class="flex items-center px-2 py-1 space-x-2 rounded dark:hover:bg-blue-800 hover:bg-blue-200">
         <x-heroicon-o-folder class="w-4 h-4 text-gray-700 dark:text-gray-400" />
         <span class="text-gray-900 dark:text-gray-200">{{ $folder['name'] }}</span>
+    </button>
+    <div x-show="open">
+        @include('livewire.bit.printer-files.loop', ['files' => $folder['children'], 'level' => true])
     </div>
-    @include('livewire.bit.printer-files.loop', ['files' => $folder['children'], 'level' => true])
 </div>
 @else
 <div class="flex items-center px-2 py-1 space-x-2">
