8b7c3a07ba63d9af4aac18522759c94002360c00
Andy Newhouse <hi@andymnewhouse.me>
2022-04-15 13:02:10 -0500

    Add quick change to printers table

--- app/Http/Livewire/Printers/Table.php
+++ app/Http/Livewire/Printers/Table.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Printers;
 
 use App\Models\Printer;
+use App\Models\Spool;
 use App\Traits\WithDelete;
 use App\Traits\WithSorting;
 use Livewire\Component;
@@ -14,15 +15,19 @@ class Table extends Component
     use WithPagination;
     use WithSorting;
 
+    public $currentPrinter;
+    public $currentSpool;
     public $perPage = 10;
     public $search = '';
     public $sortField = 'name';
     public $sortDirection = 'asc';
+    public $spoolModal = false;
 
     public function render()
     {
         return view('livewire.printers.table', [
             'rows' => $this->rows,
+            'spools' => $this->spools,
         ]);
     }
 
@@ -34,5 +39,30 @@ class Table extends Component
                 ->orWhere('model', 'LIKE', '%' . trim($this->search) . '%'))
             ->orderBy($this->sortField, $this->sortDirection)
             ->paginate($this->perPage);
+    }
+
+    public function getSpoolsProperty()
+    {
+        return Spool::all();
+    }
+
+    public function updateSpool()
+    {
+        $this->currentPrinter->spool_id = $this->currentSpool;
+        $this->currentPrinter->save();
+
+        $this->resetSpoolModal();
+    }
+
+    public function resetSpoolModal()
+    {
+        $this->reset('currentPrinter', 'currentSpool', 'spoolModal');
+    }
+
+    public function showSpoolModal($id)
+    {
+        $this->currentPrinter = $this->rows->find($id);
+        $this->currentSpool = $this->currentPrinter->spool_id;
+        $this->spoolModal = true;
     }
 }
--- resources/views/livewire/printers/table.blade.php
+++ resources/views/livewire/printers/table.blade.php
@@ -17,7 +17,12 @@
             <tr>
                 <x-table.td>{{ $printer->name }}</x-table.td>
                 <x-table.td muted>{{ $printer->model }}</x-table.td>
-                <x-table.td muted>{{ $printer->spool->name ?? 'None loaded' }}</x-table.td>
+                <x-table.td muted>
+                    <button type="button" class="flex space-x-2 items-center group rounded px-1.5 py-1 -mx-1.5 -my-1 hover:bg-gray-50 dark:hover:bg-gray-850" wire:click="showSpoolModal({{ $printer->id }})">
+                        <span>{{ $printer->spool->name ?? 'None loaded' }}</span>
+                        <x-heroicon-o-chevron-down class="invisible w-4 h-4 group-hover:visible" />
+                    </button>
+                </x-table.td>
                 <x-table.td>
                     <x-table.link href="{{ route('printers.edit', $printer) }}">Edit<span class="sr-only"> {{ $printer->name }}</x-table.link>
                 </x-table.td>
@@ -31,4 +36,25 @@
     </x-table>
 
     {{ $rows->links() }}
+
+    <x-jet-dialog-modal wire:model="spoolModal">
+        <x-slot name="title">
+            {{ __('Swap Spool') }}
+        </x-slot>
+
+        <x-slot name="content">
+            <x-form.label for="current-spool" value="Choose Spool" sr-only />
+            <x-form.select id="current-spool" wire:model="currentSpool" :options="$spools" />
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="resetSpoolModal" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-jet-secondary-button>
+
+            <x-jet-button class="ml-3" wire:click="updateSpool" wire:loading.attr="disabled">
+                {{ __('Save') }}
+            </x-jet-button>
+        </x-slot>
+    </x-jet-dialog-modal>
 </div>
