32bf1370756560449e97579bee16c349cb3e53be
Andy Newhouse <hi@andymnewhouse.me>
2022-05-29 22:37:02 -0500

    Add better seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Printer;
+use App\Models\PrintJob;
+use App\Models\Spool;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->withPersonalTeam()->create(['name' => 'Andy Newhouse', 'email' => 'hi@andymnewhouse.me']);
+        $user = User::factory()->withPersonalTeam('Newest Newhouse')->create(['name' => 'Andy Newhouse', 'email' => 'hi@andymnewhouse.me']);
+        $team = $user->currentTeam;
+
+        PrintJob::factory()->create(['name' => 'Fun']);
+
+        Printer::factory()
+            ->for($team)
+            ->for(Spool::factory()->for($team))
+            ->create([
+                'name' => 'Eevee',
+                'model' => 'Ender 3',
+                'url' => 'http://eevee.local',
+                'api_key' => 'F51A67E655A84E07B7FF62506AFE4DE0',
+            ]);
     }
 }
