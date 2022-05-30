95331726f90d8d4c494663daa83b417e6c386c40
Andy Newhouse <hi@andymnewhouse.me>
2022-05-29 22:56:31 -0500

    Dust

--- resources/views/livewire/print-jobs/form.blade.php
+++ resources/views/livewire/print-jobs/form.blade.php
@@ -25,7 +25,7 @@
             <div class="col-span-6 space-y-4">
                 @foreach ($printers as $printer)
                     <x-jet-label class="mb-1" value="{{ __('File for') }} {{ $printer->name }}" />
-                    @if(isUrlAccessible($printer->url))
+                    @if (isUrlAccessible($printer->url))
                     <div class="border border-gray-300 rounded-md dark:border-gray-700">
                         <div class="relative p-1">
                             <x-form.label for="path" :value="__('Path')" sr-only />
