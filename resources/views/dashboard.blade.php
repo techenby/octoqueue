0111affb9702f0bed8e558497b9a6dab9ae20edf
Andy Newhouse <hi@andymnewhouse.me>
2022-06-05 09:50:36 -0500

    JKS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,32 +15,7 @@
             <h2 class="text-lg font-medium leading-6 text-gray-900 dark:text-gray-200">{{ $status }}</h2>
             <div class="grid grid-cols-1 gap-6 px-6 md:grid-cols-2 md:px-0">
                 @foreach ($printers as $printer)
-                    <x-ui.card class="{{ count($printers) == 1 ? 'col-span-2' : '' }}">
-                        <x-ui.card.header>
-                            <x-slot:left>
-                                <div class="flex items-center space-x-2">
-                                    <h3 class="text-lg font-medium leading-6 text-gray-900 dark:text-gray-200">{{ $printer->name }}</h3>
-                                    <span class="text-sm text-gray-700 dark:text-gray-400">({{ $printer->status }})</span>
-                                    <button
-                                        x-data
-                                        x-tooltip="{{ $printer->spool->color }}"
-                                    >
-                                        <x-icon-filament
-                                            class="w-8 h-8"
-                                            style="fill: {{ $printer->spool->color_hex ?? 'transparent' }}"
-                                        />
-                                    </button>
-                                    <button x-data @click="Livewire.emit('pip', {{ $printer->id }})">
-                                        <x-heroicon-o-camera class="w-6 h-6 text-gray-700 dark:text-gray-400" />
-                                    </button>
-                                </div>
-                            </x-slot>
-                            <x-slot:right>
-                                <p>Next Job: <span class="text-gray-900 dark:text-gray-200">{{ $printer->nextJob->name ?? 'None' }}</span></p>
-                            </x-slot>
-                        </x-ui.card.header>
-                        @include('dashboard.' . slugify($status), ['printer' => $printer])
-                    </x-ui.card>
+                    @include('dashboard.' . slugify($status), ['printer' => $printer])
                 @endforeach
             </div>
         </div>
