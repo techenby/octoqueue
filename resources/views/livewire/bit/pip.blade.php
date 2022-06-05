8a6941130a699bb4e9ad600211c0ae1ad742d78d
Andy Newhouse <hi@andymnewhouse.me>
2022-06-05 08:45:05 -0500

    Move close pip to left corner

--- resources/views/livewire/bit/pip.blade.php
+++ resources/views/livewire/bit/pip.blade.php
@@ -1,7 +1,7 @@
 <div>
     @if ($showModal)
     <div class="absolute bottom-0 right-0 h-64 mb-4 mr-4 overflow-hidden rounded-lg shadow-lg w-96 group">
-        <button wire:click="resetModal" class="absolute top-0 right-0 mt-1 mr-1 opacity-0 group-hover:opacity-100">
+        <button wire:click="resetModal" class="absolute top-0 left-0 mt-1 ml-1 opacity-0 group-hover:opacity-100">
             <x-heroicon-o-x class="w-12 h-12 p-1 text-blue-500 rounded-full drop-shadow-lg bg-white/70" />
         </button>
         <img id="{{ $printer->id }}-webcam'" src="{{ $printer->webcam }}" />
