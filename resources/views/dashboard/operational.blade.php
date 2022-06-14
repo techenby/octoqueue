5d45b7a572a440afc6360a92b54472ebe1f0d67a
Andy Newhouse <hi@andymnewhouse.me>
2022-06-13 20:01:21 -0500

    Work on componentizing nextJob

--- resources/views/dashboard/operational.blade.php
+++ resources/views/dashboard/operational.blade.php
@@ -19,11 +19,6 @@
             </div>
         </x-slot>
     </x-ui.card.header>
-    <div class="flex items-center justify-between h-16 px-4 border-b border-gray-200 dark:border-gray-700">
-        <p class="text-gray-700 dark:text-gray-400">Next Job: <span class="text-gray-900 dark:text-gray-200">{{ $printer->nextJob->name ?? 'None' }}</span></p>
-        @if ($printer->status !== 'Printing' && $printer->nextJob !== null)
-        <button class="btn btn-sm btn-blue">Print</button>
-        @endif
-    </div>
+    <livewire:bit.next-job :printer="$printer" />
     <livewire:bit.controls :printer="$printer" />
 </x-ui.card>
