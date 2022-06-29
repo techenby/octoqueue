cc813889969a2c7a83b65c3c49a981fb463ec0ca
Andy Newhouse <hi@andymnewhouse.me>
2022-06-29 15:20:05 -0500

    Fix disabling upload buttons during temporary upload

--- resources/views/vendor/jetstream/components/dialog-modal.blade.php
+++ resources/views/vendor/jetstream/components/dialog-modal.blade.php
@@ -11,7 +11,7 @@
         </div>
     </div>
 
-    <div class="flex flex-row justify-end px-6 py-4 text-right bg-gray-100 dark:bg-gray-850">
+    <div class="flex flex-row items-center justify-end px-6 py-4 text-right bg-gray-100 dark:bg-gray-850">
         {{ $footer }}
     </div>
 </x-jet-modal>
