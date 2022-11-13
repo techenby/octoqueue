accfbbea62fa17475efc5b99e8cf98ebedf6617c
Andy Newhouse <hi@andymnewhouse.me>
2022-11-13 15:02:35 -0600

    header fix

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Team Settings') }}
-        </h2>
+        <x-layout.header :title="__('Team Settings')" />
     </x-slot>
 
     <div>
