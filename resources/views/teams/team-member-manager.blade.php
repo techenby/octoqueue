9e99bbf09ecf357e49800698ee5bdb19bb9904eb
Andy Newhouse <hi@andymnewhouse.me>
2022-11-13 15:02:26 -0600

    Dark mode fixes

--- resources/views/teams/team-member-manager.blade.php
+++ resources/views/teams/team-member-manager.blade.php
@@ -35,7 +35,7 @@
 
                             <div class="relative z-0 mt-1 border border-gray-200 rounded-lg cursor-pointer dark:border-gray-700">
                                 @foreach ($this->roles as $index => $role)
-                                    <button type="button" class="relative px-4 py-3 inline-flex w-full rounded-lg focus:z-10 focus:outline-none focus:border-cyan-300 focus:ring focus:ring-cyan-200 {{ $index > 0 ? 'border-t border-gray-200 dark:b0rder-gray-700 rounded-t-none' : '' }} {{ ! $loop->last ? 'rounded-b-none' : '' }}"
+                                    <button type="button" class="relative px-4 py-3 inline-flex w-full rounded-lg focus:z-10 focus:outline-none focus:border-cyan-300 focus:ring focus:ring-cyan-200 {{ $index > 0 ? 'border-t border-gray-200 dark:border-gray-700 rounded-t-none' : '' }} {{ ! $loop->last ? 'rounded-b-none' : '' }}"
                                                     wire:click="$set('addTeamMemberForm.role', '{{ $role->key }}')">
                                         <div class="{{ isset($addTeamMemberForm['role']) && $addTeamMemberForm['role'] !== $role->key ? 'opacity-50' : '' }}">
                                             <!-- Role Name -->
