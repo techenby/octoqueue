7c34d017e37bfe481ea1a42147e286e10aeb8cae
Andy Newhouse <hi@andymnewhouse.me>
2022-11-19 11:12:34 -0600

    JKS

--- app/Http/Livewire/Printers/Show.php
+++ app/Http/Livewire/Printers/Show.php
@@ -3,11 +3,44 @@
 namespace App\Http\Livewire\Printers;
 
 use App\Models\Printer;
+use Filament\Notifications\Notification;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class Show extends Component
 {
     public Printer $printer;
+
+    public $tools = [];
+
+    public $rules = [
+        'tools.*.material_id' => 'nullable',
+    ];
+
+    public function mount()
+    {
+        $this->tools = $this->printer->tools->mapWithKeys(function ($tool) {
+            return [$tool->id => ['name' => $tool->name, 'material_id' => $tool->material_id]];
+        })->toArray();
+    }
+
+    public function updatedTools($value, $key)
+    {
+        $this->validate();
+
+        $tool = $this->printer->tools->find(Str::before($key, '.'));
+
+        $tool->update([
+            'material_id' => ($value === '') ? null : $value
+        ]);
+
+        Notification::make()
+            ->title("{$tool->name} updated successfully.")
+            ->success()
+            ->duration(5000)
+            ->send();
+    }
 
     public function render()
     {
@@ -17,6 +50,7 @@ class Show extends Component
                 ['label' => 'Printers', 'route' => 'printers'],
                 ['label' => $this->printer->name],
             ],
+            'materialOptions' => auth()->user()->currentTeam->materials->pluck('name', 'id'),
         ]);
     }
 
--- resources/views/livewire/printers/show.blade.php
+++ resources/views/livewire/printers/show.blade.php
@@ -7,7 +7,29 @@
     </x-layout.header>
 </x-slot>
 
-<div>
+<div class="space-y-4">
     @includeWhen($printer->status === 'offline', 'livewire.printers.partials.offline-alert')
     @includeWhen($printer->status === 'closed', 'livewire.printers.partials.closed-alert')
+
+    <div class="grid grid-cols-5 gap-4">
+        <div id="tools" class="col-span-2 overflow-hidden bg-white rounded-md shadow dark:bg-gray-800">
+            <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+                @foreach($tools as $id => $tool)
+                <li class="flex items-center justify-between px-6 py-4 space-x-4">
+                    <label for="tool.{{ $id }}.material_id" class="inline text-gray-700 dark:text-gray-400">{{ $tool['name'] }}</label>
+                    <select name="tool.{{ $id }}.material_id" id="tool.{{ $id }}.material_id"
+                        wire:model="tools.{{ $id }}.material_id"
+                        class="block w-full text-gray-900 transition duration-75 border-gray-300 rounded-lg shadow-sm focus:border-primary-500 focus:ring-1 focus:ring-inset focus:ring-primary-500 disabled:opacity-70 dark:bg-gray-700 dark:text-white dark:focus:border-primary-500 dark:border-gray-600"
+                    >
+                        <option value="">Select an option</option>
+                        @foreach($materialOptions as $value => $option)
+                            <option value="{{ $value }}">{{ $option }}</option>
+                        @endforeach
+                    </select>
+                </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
+
 </div>
