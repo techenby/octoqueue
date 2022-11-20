176181177a07d7edf0d11dad8ea379c588625319
Andy Newhouse <hi@andymnewhouse.me>
2022-11-19 18:26:12 -0600

    Add relationships to Tools

--- app/Models/Tool.php
+++ app/Models/Tool.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tool extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function printer(): BelongsTo
+    {
+        return $this->belongsTo(Printer::class);
+    }
+
+    public function material(): BelongsTo
+    {
+        return $this->belongsTo(Material::class);
+    }
 }
