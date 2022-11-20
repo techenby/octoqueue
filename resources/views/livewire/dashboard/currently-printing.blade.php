fee897ae6312efb0f7c5f7a9ced5600a5c51d2de
Andy Newhouse <hi@andymnewhouse.me>
2022-11-19 18:26:22 -0600

    JKS

--- resources/views/livewire/dashboard/currently-printing.blade.php
+++ resources/views/livewire/dashboard/currently-printing.blade.php
@@ -6,10 +6,19 @@
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
             @foreach($printers as $printer)
             <li class="p-4">
-                <div class="flex items-center space-x-4">
-                    <div class="flex-1 min-w-0">
-                        <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-200">{{ $printer->name }}</p>
-                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">{{ $printer->model }}</p>
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center min-w-0 space-x-4">
+                        <div>
+                            <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-200">{{ $printer->name }}</p>
+                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">{{ $printer->model }}</p>
+                        </div>
+                        @foreach($printer->tools as $tool)
+                        <div class="space-x-1">
+                            @if($tool->material_id !== null)
+                                <x-icon.filament x-data :x-tooltip.raw="$tool->material->name" class="w-12 h-12 text-gray-700 dark:text-gray-400" :fill="$tool->material->color_hex" />
+                            @endif
+                        </div>
+                        @endforeach
                     </div>
                     <div>
                         <a href="{{ route('printers.show', $printer) }}" class="inline-flex items-center rounded-full border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 px-2.5 py-0.5 text-sm font-medium leading-5 text-gray-700 dark:text-gray-400 shadow-sm hover:bg-gray-50 dark:hover:bg-gray-850">View</a>
