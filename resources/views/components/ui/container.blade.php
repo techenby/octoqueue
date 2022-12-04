eb3ac5354ea32cfe57ef22bd195b20a4ade56b77
Andy Newhouse <hi@andymnewhouse.me>
2022-12-04 13:00:05 -0600

    Allow full width container

--- resources/views/components/ui/container.blade.php
+++ resources/views/components/ui/container.blade.php
@@ -1,5 +1,7 @@
+@props(['full' => false])
+
 <main class="py-12">
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="{{ $full ? 'px-12' : 'mx-auto max-w-7xl' }} sm:px-6 lg:px-8">
         {{ $slot }}
     </div>
 </main>
