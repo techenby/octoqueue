8577c766077788e53597dd06b76bd40d63edded7
Andy Newhouse <hi@andymnewhouse.me>
2022-12-09 14:20:13 -0600

    Add fakes for temps component to render properly

--- tests/Feature/Livewire/Printers/ShowTest.php
+++ tests/Feature/Livewire/Printers/ShowTest.php
@@ -7,6 +7,7 @@ use App\Models\Printer;
 use App\Models\Tool;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -14,11 +15,59 @@ class ShowTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * This was taken from the OctoPrint documentation on 2022-12-06
+     * https://docs.octoprint.org/en/master/api/printer.html#get--api-printer
+     */
+    public $printerResponse = [
+        'temperature' => [
+            'tool0' => [
+                'actual' => 214.8821,
+                'target' => 220.0,
+                'offset' => 0,
+            ],
+            'tool1' => [
+                'actual' => 25.3,
+                'target' => null,
+                'offset' => 0,
+            ],
+            'bed' => [
+                'actual' => 50.221,
+                'target' => 70.0,
+                'offset' => 5,
+            ],
+        ],
+        'sd' => [
+            'ready' => true,
+        ],
+        'state' => [
+            'text' => 'Operational',
+            'flags' => [
+                'operational' => true,
+                'paused' => false,
+                'printing' => false,
+                'cancelling' => false,
+                'pausing' => false,
+                'sdReady' => true,
+                'error' => false,
+                'ready' => true,
+                'closedOrError' => false,
+            ],
+        ],
+    ];
+
     /** @test */
     public function the_component_can_render()
     {
+        Http::fake([
+            'bulbasaur.local/api/printer' => Http::response($this->printerResponse),
+            'bulbasaur.local/api/printer/bed' => Http::response([], 204),
+        ]);
+
         $user = User::factory()->withPersonalTeam()->create();
-        $printer = Printer::factory()->for($user->currentTeam)->createQuietly();
+        $printer = Printer::factory()->for($user->currentTeam)->createQuietly([
+            'url' => 'http://bulbasaur.local',
+        ]);
 
         Livewire::actingAs($user)->test(Show::class, ['printer' => $printer])
             ->assertStatus(200);
@@ -27,11 +76,18 @@ class ShowTest extends TestCase
     /** @test */
     public function can_delete_pritner()
     {
+        Http::fake([
+            'bulbasaur.local/api/printer' => Http::response($this->printerResponse),
+            'bulbasaur.local/api/printer/bed' => Http::response([], 204),
+        ]);
+
         $user = User::factory()->withPersonalTeam()->create();
         $printer = Printer::factory()
             ->for($user->currentTeam)
             ->has(Tool::factory())
-            ->createQuietly();
+            ->createQuietly([
+                'url' => 'http://bulbasaur.local',
+            ]);
 
         Livewire::actingAs($user)->test(Show::class, ['printer' => $printer])
             ->call('deletePrinter')
