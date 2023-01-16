9823ab9bcb59ffd987a327c5a317825bfca801ba
Andy Newhouse <hi@andymnewhouse.me>
2023-01-16 13:49:50 -0600

    Remove defining widgets

--- config/filament.php
+++ config/filament.php
@@ -140,9 +140,7 @@ return [
         'namespace' => 'App\\Filament\\Widgets',
         'path' => app_path('Filament/Widgets'),
         'register' => [
-            Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
-            CurrentlyPrinting::class,
+            //
         ],
     ],
 
