3a1477815de05dec77f433a352acb88b69e008ba
techenby <techenby@users.noreply.github.com>
2023-01-17 06:26:07 +0000

    Pint

--- app/Filament/Resources/TeamResource/Pages/CreateTeam.php
+++ app/Filament/Resources/TeamResource/Pages/CreateTeam.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\TeamResource\Pages;
 
 use App\Filament\Resources\TeamResource;
-use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTeam extends CreateRecord
