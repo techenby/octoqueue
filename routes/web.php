1ebcfb863dc87a1424d3b5c849658c95079aba35
Andy Newhouse <hi@andymnewhouse.me>
2023-01-17 00:09:14 -0600

    Remove dashboard from routes

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\MarketingController::class, 'welcome'])->name('welcome');
 Route::get('/marketing', [App\Http\Controllers\MarketingController::class, 'marketing'])->name('marketing');
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', App\Http\Livewire\Dashboard::class)->name('dashboard');
-});
