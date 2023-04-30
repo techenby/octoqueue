926323e474f01bb79c245122e210a13f04493dfc
Andy Newhouse <hi@andymnewhouse.me>
2023-04-30 18:06:37 -0500

    Fix materials column

--- resources/views/filament/tables/columns/materials.blade.php
+++ resources/views/filament/tables/columns/materials.blade.php
@@ -1,5 +1,3 @@
 <div class="filament-tables-text-column px-3 py-3">
-    @foreach($getRecord()->tools as $tool)
-    <x-icon.filament :x-tooltip.raw="$tool->name ?? '' .' - ' . $tool->material->name ?? ''" class="w-12 h-12 text-gray-900 dark:text-gray-200" :fill="$tool->material->color_hex ?? '#000000'" />
-    @endforeach
+    <x-icon.filament :x-tooltip.raw="$getRecord()->material->name ?? ''" class="w-12 h-12 text-gray-900 dark:text-gray-200" :fill="$getRecord()->material->color_hex ?? '#000000'" />
 </div>
