ba96d1a2993ec017fd85065dbf2ccc72fa687527
Andy Newhouse <hi@andymnewhouse.me>
2023-05-14 16:51:36 -0500

    Add filters

--- app/Filament/Widgets/PrintQueue.php
+++ app/Filament/Widgets/PrintQueue.php
@@ -12,6 +12,7 @@ use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Widgets\TableWidget as BaseWidget;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -69,6 +70,18 @@ class PrintQueue extends BaseWidget
                 EditAction::make(),
                 DeleteAction::make(),
             ]),
+        ];
+    }
+
+    protected function getTableFilters(): array
+    {
+        return [
+            SelectFilter::make('color_hex')
+                ->label('Color')
+                ->multiple()
+                ->options(auth()->user()->currentTeam->materials->pluck('name', 'color_hex')),
+            SelectFilter::make('printType')->relationship('printType', 'name'),
+            SelectFilter::make('user')->relationship('user', 'name'),
         ];
     }
 
